995daa90dd889cd220222d76e27e549dad96d3bc
Marek Zając <marek.zajac1990@gmail.com>
2019-10-13 00:02:07 +0200

    wyglad, artzkoly , js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const age='29';
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const heading = document.querySelector('.header_titel--js');
+console.log(heading.innerHTML);
+heading.innerHTML = 'Posdro JS';
+
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
